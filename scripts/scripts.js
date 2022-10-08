5e8b4e0a72df4f4a02a74b0476261415b87ddace
JalenCameron <109976308+JalenCameron@users.noreply.github.com>
2022-10-08 02:33:32 -0700

    Checking whether the current player won

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -27,8 +27,39 @@ function handlePlayerChange() {
 
 };
 
-function handleResults() {
+// Determing the conditions that need to be met in order to win 
+const winningConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
 
+function handleResults() {
+    let roundWon = false;
+    for (let i = 0; i<=7; i++) {
+        const winCondition = winningConditions[i];
+        let a = gameState[winCondition[0]];
+        let b = gameState[winCondition[1]];
+        let c = gameState[winCondition[2]];
+        if (a === '' || b === '' || c === '') {
+            continue;
+        }
+        if (a === b && b === c) {
+            roundWon = true;
+            break;
+        }
+    };
+
+    if (roundWon) {
+        displayStatus.innerHTML = winningMessage();
+        gameActive = false;
+        return;
+    }
 };
 
 function handleCellClick(clickedCellEvent) {
